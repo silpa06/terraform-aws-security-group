06a33c01e881abcc35a18cef3129ec5b14abbaa7
Silpa <silpa.jr@gmail.com>
2024-09-27 22:14:36 +0800

    aws-security-group

---
+++ locals.tf
@@ -0,0 +1,3 @@
+locals {
+  sg_final_name = "${var.proect_name}-${var.environment}-${var.sg_name}"
+}
---
+++ variables.tf
@@ -0,0 +1,25 @@
+variable "proect_name" {
+    type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "sg_name" {
+  type = string
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "common_tags" {
+  type = map 
+  default = {}
+}
+
+variable "sg_tags" {
+  type = map 
+  default = {}
+}
